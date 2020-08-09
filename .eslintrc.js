3cdc4efd42a34f0c00ed306825d709827b9195b2
abumalick <abdullah.hilson@gmail.com>
2020-08-09 10:28:22 +0100

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 0,
+  },
+}
